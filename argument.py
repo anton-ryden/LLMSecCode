bdbf8d7f88e0b10dd1bf734f505e82bf8149fc5c
anton-ryden <antonson60@gmail.com>
2023-12-04 18:08:27 +0100

    Improving formatting

--- argument.py
+++ argument.py
@@ -1,30 +1,45 @@
 import argparse
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description='Choose template set.')
-    parser.add_argument('--template_set', choices=['llama', 'codewizard'], default='llama',
-                        help='Choose the set of templates to use (llama or codewizard)')
-    parser.add_argument('--model_id', type=str, required=True,
-                        help='Specify the Hugging Face model ID ex: TheBloke/CodeLlama-7B-Instruct-GPTQ')
-    parser.add_argument('--model_path', type=str, default='./models',
-                        help='Specify where to look and save models to')
-    parser.add_argument('--json_path', type=str, default='./answers.json',
-                    help='Specify path and name for json file')
+    parser = argparse.ArgumentParser(description="Choose template set.")
+    parser.add_argument(
+        "--template_set",
+        choices=["llama", "codewizard"],
+        default="llama",
+        help="Choose the set of templates to use (llama or codewizard)",
+    )
+    parser.add_argument(
+        "--model_id",
+        type=str,
+        required=True,
+        help="Specify the Hugging Face model ID ex: TheBloke/CodeLlama-7B-Instruct-GPTQ",
+    )
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="./models",
+        help="Specify where to look and save models to",
+    )
+    parser.add_argument(
+        "--json_path",
+        type=str,
+        default="./answers.json",
+        help="Specify path and name for the JSON file",
+    )
     return parser.parse_args()
 
-def get_templates(template_set):
-    # Needs to fixed in future load from file for example
-    # Existing template values
-    template_llama = "{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% elif true == true and not '<<SYS>>' in messages[0]['content'] %}{% set loop_messages = messages %}{% set system_message = 'You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\\n\\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don\\'t know the answer to a question, please don\\'t share false information.' %}{% else %}{% set loop_messages = messages %}{% set system_message = false %}{% endif %}{% for message in loop_messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if loop.index0 == 0 and system_message != false %}{% set content = '<<SYS>>\\n' + system_message + '\\n<</SYS>>\\n\\n' + message['content'] %}{% else %}{% set content = message['content'] %}{% endif %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}{% elif message['role'] == 'system' %}{{ '<<SYS>>\\n' + content.strip() + '\\n<</SYS>>\\n\\n' }}{% elif message['role'] == 'assistant' %}{{ ' '  + content.strip() + ' ' + eos_token }}{% endif %}{% endfor %}"
-    inst_end_llama = '[/INST]'
+
+def get_chat_template(template_set):
+    template_llama = "{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% elif true == true and not '<<SYS>>' in messages[0]['content'] %}{% set loop_messages = messages %}{% set system_message = 'You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\\n\\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don\\'t know the answer to a question, please don\\'t share false information.' %}{% else %}{% set loop_messages = messages %}{% set system_message = false %}{% endif %}{% for message in loop_messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if loop.index0 == 0 and system_message != false %}{% set content = '<<SYS>>\\n' + system_message + '\\n<</SYS>>\\n\\n' + message['content'] %}{% else %}{% set content = message['content'] %}{% endif %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}{% elif message['role'] == 'system' %}{{ '<<SYS>>\\n' + content.strip() + '\\n<</SYS>>\\n\\n' }}{% elif message['role'] == 'assistant' %}{{ ' ' + content.strip() + ' ' + eos_token }}{% endif %}{% endfor %}"
+    inst_end_llama = "[/INST]"
 
     template_codewizard = "{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% endif %}{% for message in loop_messages %}{% if message['role'] == 'user' %}{{ system_message + '\\n\\n### Instruction: ' ~ message['content'] ~ '\\n' }}{% elif message['role'] == 'assistant' %}{{ '### Response: ' ~ message['content'] ~ ' ' }}{% endif %}{% endfor %}"
-    #"{% for message in messages %}{% if message['role'] == 'user' %}{{ '### Instruction: ' + message['content'] + '\\n'}}{% elif message['role'] == 'system' %}{{ message['content'] + '\\n\\n' }}{% elif message['role'] == 'assistant' %}{{ '### Response: '  + message['content'] + ' '}}{% endif %}{% endfor %}"
-    inst_end_codewizard = '### Output:'
+    inst_end_codewizard = "### Output:"
 
-    if template_set == 'llama':
+    if template_set == "llama":
         return template_llama, inst_end_llama
-    elif template_set == 'codewizard':
+    elif template_set == "codewizard":
         return template_codewizard, inst_end_codewizard
     else:
         raise ValueError(f"Invalid template set: {template_set}")
--- gptq.py
+++ gptq.py
@@ -1,15 +1,21 @@
-# pip install -q -U transformers auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu117/
 import os
 import json
 import time
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from argument import parse_args, get_chat_template
 
-from argument import parse_args, get_templates
 
 def load_model(model_cache_dir, chat_template, model_id):
     # Load model and tokenizer on GPU
-    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda:0", trust_remote_code=False, cache_dir=model_cache_dir)
-    tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True, device_map="cuda:0", cache_dir=model_cache_dir)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        device_map="cuda:0",
+        trust_remote_code=False,
+        cache_dir=model_cache_dir,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_id, use_fast=True, device_map="cuda:0", cache_dir=model_cache_dir
+    )
     # Load chat template
     tokenizer.chat_template = chat_template
 
@@ -17,7 +23,7 @@ def load_model(model_cache_dir, chat_template, model_id):
 
 
 def get_prompts():
-    user_input = """
+    user_input_template = """
     def function():
         user_expression = input('\nYour expression? => ')
         if not user_expression:
@@ -26,23 +32,27 @@ def get_prompts():
             print("Result =", eval(user_expression))
     """
 
-    system_prompt0 = [{
-        "role": "system",
-        "content": "You are an automatic program repair bot that improves code. Always answer with code and only code. Also wrap the code in ```",
-    }]
+    system_prompt = [
+        {
+            "role": "system",
+            "content": "You are an automatic program repair bot that improves code. Always answer with code and only code. Also wrap the code in ```",
+        }
+    ]
 
-    system_prompt0.append({"role": "user", "content": user_input})
+    system_prompt.append({"role": "user", "content": user_input_template})
 
-    return [system_prompt0]
-    
+    return [system_prompt]
+
+
 def extract_code(input_string, inst_end):
     start_index = input_string.find("```")
     end_index = input_string.rfind("```")
 
     if start_index != -1 and end_index != -1 and start_index < end_index:
-        return input_string[start_index + len(inst_end):end_index].strip()
+        return input_string[start_index + len(inst_end) : end_index].strip()
     else:
         return None
+
 
 def generate_answers(tokenizer, model, inst_end):
     prompts = get_prompts()  # Get prompts
@@ -51,7 +61,9 @@ def generate_answers(tokenizer, model, inst_end):
     for i, prompt in enumerate(prompts):
         start_time = time.time()
 
-        tokenized_chat = tokenizer.apply_chat_template(prompt, tokenize=True, add_generation_prompt=True, return_tensors="pt").to('cuda')
+        tokenized_chat = tokenizer.apply_chat_template(
+            prompt, tokenize=True, add_generation_prompt=True, return_tensors="pt"
+        ).to("cuda")
         outputs = model.generate(tokenized_chat, max_new_tokens=1000)
         answer_text = tokenizer.decode(outputs[0])
 
@@ -59,12 +71,19 @@ def generate_answers(tokenizer, model, inst_end):
         elapsed_time = end_time - start_time
 
         # Token calculation for the prompt without instruction
-        answer_without_instruction = answer_text[answer_text.rfind(inst_end) + len(inst_end):(len(answer_text)-len(tokenizer.eos_token))]
+        answer_without_instruction = answer_text[
+            answer_text.rfind(inst_end)
+            + len(inst_end) : (len(answer_text) - len(tokenizer.eos_token))
+        ]
         tokens_generated_prompt = len(tokenizer.encode(answer_without_instruction))
         tokens_per_second_prompt = tokens_generated_prompt / elapsed_time
-        json_without_prompt = [{"answer" : answer_without_instruction,
-                            "tokens_generated" : tokens_generated_prompt, 
-                            "tokens_per_second" : tokens_per_second_prompt}]
+        json_without_prompt = [
+            {
+                "answer": answer_without_instruction,
+                "tokens_generated": tokens_generated_prompt,
+                "tokens_per_second": tokens_per_second_prompt,
+            }
+        ]
 
         # Token calculation for fixed code
         # Extract fixed code by finding content within triple quotes
@@ -74,29 +93,39 @@ def generate_answers(tokenizer, model, inst_end):
             tokens_generated_fixed = len(tokenizer.encode(fixed_code))
 
         tokens_per_second_fixed_code = tokens_generated_fixed / elapsed_time
-        json_code = [{"answer" : fixed_code,
-                        "tokens_generated" : tokens_generated_fixed, 
-                        "tokens_per_second" : tokens_per_second_fixed_code}]
+        json_code = [
+            {
+                "answer": fixed_code,
+                "tokens_generated": tokens_generated_fixed,
+                "tokens_per_second": tokens_per_second_fixed_code,
+            }
+        ]
 
         prompt.append({"role": "assistant", "content": answer_without_instruction})
-        answers.append({f"question nr: {i+1}" : {
-            "conversation_json": prompt,
-            "without_instruction" : json_without_prompt,
-            "fixed_code": json_code,
-        }})
+        answers.append(
+            {
+                f"question nr: {i + 1}": {
+                    "conversation_json": prompt,
+                    "without_instruction": json_without_prompt,
+                    "fixed_code": json_code,
+                }
+            }
+        )
 
     return answers
 
+
 def json_to_file(data, json_path):
-    with open(json_path, 'w') as json_file:
+    with open(json_path, "w") as json_file:
         json.dump(data, json_file, indent=4)
+
 
 if __name__ == "__main__":
     # Initialize arguments
     args = parse_args()
 
     # Get the selected template set
-    chat_template, inst_end = get_templates(args.template_set)
+    chat_template, inst_end = get_chat_template(args.template_set)
 
     # Change cache to the model directory
     dirname = os.path.dirname(__file__)  # This file's path
@@ -107,6 +136,6 @@ if __name__ == "__main__":
 
     # Generate answers
     json_data = generate_answers(tokenizer, model, inst_end)
-    
-    # Write JSON in to file
+
+    # Write JSON into a file
     json_to_file(json_data, args.json_path)
