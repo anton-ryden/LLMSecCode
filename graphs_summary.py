c7b9d289769c7f57f74399e3825524f8103d7d2e
ekan9 <erik.naslund99@gmail.com>
2024-05-15 16:45:06 +0200

    Updated graphs_summary.py

--- graphs_summary.py
+++ graphs_summary.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import pandas as pd
 
+
 def merge_json_files(input_folder, output_file):
     merged_data = {}
 
@@ -17,15 +18,13 @@ def merge_json_files(input_folder, output_file):
                 json_files.append(os.path.join(root, file))
     json_files.sort()  # Sort the list of JSON files
 
-    
-
     # Iterate over each JSON file and merge the data
     for json_file in json_files:
         with open(json_file) as file:
             data = json.load(file)
             filename = os.path.basename(json_file)
             filename = os.path.splitext(filename)[0]
-            conversation_type = filename.split('_')[0]
+            conversation_type = filename.split("_")[0]
             if conversation_type not in merged_data:
                 merged_data[conversation_type] = []
 
@@ -38,6 +37,7 @@ def merge_json_files(input_folder, output_file):
     with open(output_file, "w") as output:
         json.dump(merged_data, output, indent=6)
 
+
 def plot_errors(result_directory, dataset):
 
     all_error_info = {}
@@ -46,7 +46,7 @@ def plot_errors(result_directory, dataset):
     for json_file in os.listdir(result_directory):
         filename = os.path.basename(json_file)
         filename = os.path.splitext(filename)[0]
-        model_name = filename.split('_')[0]
+        model_name = filename.split("_")[0]
         if json_file.endswith(".json"):
             json_file_path = os.path.join(result_directory, json_file)
             with open(json_file_path) as file:
@@ -66,22 +66,26 @@ def plot_errors(result_directory, dataset):
             all_error_info[model_name] = error_info
 
     # Plotting Errors
-    df = pd.DataFrame.from_dict({(i, j): all_error_info[i][j] 
-                             for i in all_error_info.keys() 
-                             for j in all_error_info[i].keys()},
-                            orient='index')
+    df = pd.DataFrame.from_dict(
+        {
+            (i, j): all_error_info[i][j]
+            for i in all_error_info.keys()
+            for j in all_error_info[i].keys()
+        },
+        orient="index",
+    )
 
     # Plotting using Pandas plot
     fig, ax = plt.subplots(figsize=(12, 6))
-    df.unstack().plot(kind='bar', ax=ax, width=0.8)
+    df.unstack().plot(kind="bar", ax=ax, width=0.8)
 
-    ax.set_xlabel('Models')
-    ax.set_ylabel('Number of errors')
-    ax.set_title(f'Amount of Errors for {dataset}')
-    ax.legend(title='Errors', bbox_to_anchor=(1.05, 1), loc='upper left')
+    ax.set_xlabel("Models")
+    ax.set_ylabel("Number of errors")
+    ax.set_title(f"Amount of Errors for {dataset}")
+    ax.legend(title="Errors", bbox_to_anchor=(1.05, 1), loc="upper left")
 
     plt.savefig(f"./graphs/{dataset}_error_plot.png", bbox_inches="tight")
-    
+
 
 def plot_pass_at_k(result_directory, dataset):
 
@@ -91,7 +95,7 @@ def plot_pass_at_k(result_directory, dataset):
     for json_file in os.listdir(result_directory):
         filename = os.path.basename(json_file)
         filename = os.path.splitext(filename)[0]
-        model_name = filename.split('_')[0]
+        model_name = filename.split("_")[0]
         if json_file.endswith(".json"):
             json_file_path = os.path.join(result_directory, json_file)
             with open(json_file_path) as file:
@@ -111,29 +115,105 @@ def plot_pass_at_k(result_directory, dataset):
             all_pass_at_k_info[model_name] = pass_at_k_info
 
     # Plotting Pass@k
-    df = pd.DataFrame.from_dict({(i, j): all_pass_at_k_info[i][j] 
-                             for i in all_pass_at_k_info.keys() 
-                             for j in all_pass_at_k_info[i].keys()},
-                            orient='index')
+    df = pd.DataFrame.from_dict(
+        {
+            (i, j): all_pass_at_k_info[i][j]
+            for i in all_pass_at_k_info.keys()
+            for j in all_pass_at_k_info[i].keys()
+        },
+        orient="index",
+    )
 
     # Plotting using Pandas plot
     fig, ax = plt.subplots(figsize=(12, 6))
-    df.unstack().plot(kind='bar', ax=ax, width=0.8)
+    df.unstack().plot(kind="bar", ax=ax, width=0.8)
 
-    ax.set_xlabel('Models')
-    ax.set_ylabel('Pass@k (%)')
-    ax.set_title(f'Pass@k Values for {dataset}')
-    ax.legend(title='Pass@k', bbox_to_anchor=(1.05, 1), loc='upper left')
+    ax.set_xlabel("Models")
+    ax.set_ylabel("Pass@k (%)")
+    ax.set_title(f"Pass@k Values for {dataset}")
+    ax.legend(title="Pass@k", bbox_to_anchor=(1.05, 1), loc="upper left")
 
     plt.savefig(f"./graphs/{dataset}_pass_at_k_plot.png", bbox_inches="tight")
 
 
+def combine_dataset_pass1(result_directory, datasets: list):
+
+    all_pass_at_1_info = {}
+
+    # Get the pass@k values of each model and config
+    for json_file in os.listdir(result_directory):
+        filename = os.path.basename(json_file)
+        filename = os.path.splitext(filename)[0]
+        model_name = filename.split("_")[0]
+        all_pass_at_1_info[model_name] = {}
+        for dataset in datasets:
+
+            if json_file.endswith(".json"):
+                json_file_path = os.path.join(result_directory, json_file)
+                with open(json_file_path) as file:
+                    data = json.load(file)
+
+                pass_at_k_info = {}
+                for config_type, config_data in data.items():
+                    for entry in config_data:
+                        if entry["Name"] == dataset:
+
+                            for key, value in entry["Statistics"]["0"].items():
+                                if key.startswith("Pass@1"):
+                                    pass_at_k_info[key] = value
+
+                            all_pass_at_1_info[model_name][dataset] = pass_at_k_info
+
+    # Plotting the pass@1 scores
+    num_datasets = len(datasets)
+    bar_width = 0.35  # Width of each bar
+    index = np.arange(len(all_pass_at_1_info))  # Index for each model
+    bar_positions = [index + i * bar_width for i in range(num_datasets)]
+
+    plt.figure(figsize=(10, 6))  # Adjust the size here
+
+    for i, dataset in enumerate(datasets):
+        pass_at_1_scores = [
+            all_pass_at_1_info[model_name][dataset][
+                "Pass@1"
+            ]  # assuming each dataset has one pass@1 score
+            for model_name in all_pass_at_1_info.keys()
+        ]
+
+        plt.bar(
+            bar_positions[i],
+            pass_at_1_scores,
+            bar_width,
+            align="center",
+            alpha=0.7,
+            label=dataset,
+        )
+
+        # Annotate each bar with its height (pass@1 score)
+        for x, y in zip(bar_positions[i], pass_at_1_scores):
+            plt.text(x, y, f"{y:.2f}", ha="center", va="bottom", fontsize=7.5)
+
+    plt.xlabel("Model Name")
+    plt.ylabel("Pass@1 Score")
+    plt.title("Pass@1 Scores for Models")
+    plt.xticks(
+        index + (num_datasets - 1) * bar_width / 2,
+        all_pass_at_1_info.keys(),
+        rotation=45,
+        ha="right",
+    )
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+    plt.savefig(f"./graphs/combined_pass_at_1_plot.png", bbox_inches="tight")
+
+
 if __name__ == "__main__":
-    
 
     # Get the absolute path of the current file
     current_file_path = os.path.dirname(os.path.abspath(__file__))
-    default_result_path = os.path.join(current_file_path, "results/default/")
+    default_result_path = os.path.join(current_file_path, "results/code_eval/")
     merged_dir = os.path.join(default_result_path, "merged")
 
     for model_name in os.listdir(default_result_path):
@@ -142,19 +222,18 @@ if __name__ == "__main__":
 
             merge_json_files(
                 model_path,
-                default_result_path  + "/merged/" + model_name + "_merged.json",
+                default_result_path + "/merged/" + model_name + "_merged.json",
             )
 
     # Create directory for saving plotting results
     graph_directory = os.path.join(current_file_path, "graphs")
-    os.makedirs(graph_directory, exist_ok=True)  
+    os.makedirs(graph_directory, exist_ok=True)
 
     # Define dataset (QuixBugs Python, QuixBugs Java, HumanEval)
-    datasets = ["QuixBugs Python", "HumanEval"]      
-    
-    # Plot graphs
-    for dataset in datasets:
-        plot_pass_at_k(merged_dir, dataset)
-        plot_errors(merged_dir, dataset)
+    datasets = ["HumanEval", "QuixBugs Java"]
 
-    
+    combine_dataset_pass1(merged_dir, datasets)
+    # Plot graphs
+    # for dataset in datasets:
+    #    plot_pass_at_k(merged_dir, dataset)
+    #    plot_errors(merged_dir, dataset)
