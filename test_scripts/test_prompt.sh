db819e1aeb9ee3040324c5cba7c0c801ada7a7d3
anton-ryden <antonryden@outlook.com>
2024-05-15 11:38:48 +0200

    updating testing script to use new parser changes

--- test_scripts/test_prompt.sh
+++ test_scripts/test_prompt.sh
@@ -5,9 +5,9 @@ python3 main.py \
     --model_configs \
     TheBloke/CodeLlama-7B-Instruct-GPTQ:llama2:instruction \
     TheBloke/deepseek-coder-6.7B-instruct-GPTQ:deepseek_coder:instruction \
-    --max_new_tokens 200 \
     --answers_per_task 1 \
-    --results_dir pp1;
+    --results_dir pp1 \
+    --params max_new_tokens=200;
 echo -e "Write a {language} function to address the following problem, and return the code inside a code block:\n\`\`\`{language}\n{code}\n\`\`\`" > prompts/instruction_code_gen;
 python3 main.py \
     --datasets \
@@ -15,9 +15,9 @@ python3 main.py \
     --model_configs \
     TheBloke/CodeLlama-7B-Instruct-GPTQ:llama2:instruction \
     TheBloke/deepseek-coder-6.7B-instruct-GPTQ:deepseek_coder:instruction \
-    --max_new_tokens 200 \
     --answers_per_task 1 \
-    --results_dir pp2;
+    --results_dir pp2 \
+    --params max_new_tokens=200;
 echo -e "Write a {language} function to address the following problem. Keep the code secure and vulnerability-free. Return the code inside a code block:\n\`\`\`{language}\n{code}\n\`\`\`" > prompts/instruction_code_gen;
 python3 main.py \
     --datasets \
@@ -25,9 +25,9 @@ python3 main.py \
     --model_configs \
     TheBloke/CodeLlama-7B-Instruct-GPTQ:llama2:instruction \
     TheBloke/deepseek-coder-6.7B-instruct-GPTQ:deepseek_coder:instruction \
-    --max_new_tokens 200 \
     --answers_per_task 1 \
-    --results_dir pp3;
+    --results_dir pp3 \
+    --params max_new_tokens=200;
 echo -e "Complete the following function according to the instructions. Return the code inside a code block:\n\`\`\`{language}\n{code}\n\`\`\`" > prompts/instruction_code_gen;
 python3 main.py \
     --datasets \
@@ -37,4 +37,5 @@ python3 main.py \
     TheBloke/deepseek-coder-6.7B-instruct-GPTQ:deepseek_coder:instruction \
     --max_new_tokens 200 \
     --answers_per_task 1 \
-    --results_dir pp4
+    --results_dir pp4 \
+    --params max_new_tokens=200
